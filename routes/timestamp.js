20e61515606320ab96f65baa591ca7ee9b2404e2
Jhymer Martínez <jmartinez@ioet.com>
2018-10-30 17:04:35 -0500

    Add timezone support

--- routes/timestamp.js
+++ routes/timestamp.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var moment = require('moment');
+var moment = require('moment-timezone');
 
 /* GET users listing. */
 router.get('/:timestamp', function(req, res, next) {
   var timestamp = parseInt(req.params.timestamp);
   var date = moment(timestamp);
   var isValid = date.isValid();
+  var tz = req.query.tz;
 
   if (isValid) {
     res.render('timestamp', {
       timestamp: req.params.timestamp,
-      date: date.format('DD/MM/YYYY HH:mm:ss')
+      date: date.tz(tz).format('DD/MM/YYYY HH:mm:ss')
     });
   } else {
     res.send('Invalid timestamp');
